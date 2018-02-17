df0b0bf55b0ce0d4507f366ea8d7be839d7776b1
furkle <furkle@furkleindustries.com>
2018-02-17 01:44:18 -0500

    Fixed syntax errors in main.js.

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 // In the main process.
-const {BrowserWindow} = require('electron')
+const {
+  app,
+  BrowserWindow,
+} = require('electron');
 
-let win = new BrowserWindow({width: 800, height: 600, frame: false, resizable: false})
-win.on('closed', () => {
-  win = null
-})
-
-// Or load a local HTML file
-win.loadURL(`file://${__dirname}/app/index.html`)
-
-//ready-to-show; should prevent slow load i.e. issue in Safari
-const {BrowserWindow} = require('electron')
-let win = new BrowserWindow({show: false})
-win.once('ready-to-show', () => {
-  win.show()
-})
-
- // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+let win;
+app.on('ready', () => {
+  win = new BrowserWindow();
+  
+  // Load a local HTML file.
+  win.loadURL(`file://${__dirname}/index.html`);
+  
+  // ready-to-show; should prevent slow load i.e. issue in Safari.
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+  
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
 });
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q.
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
